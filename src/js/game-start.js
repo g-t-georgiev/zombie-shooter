17cbc54f7f604bae1fa9e5fbbefeea197259b4c5
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2024-03-31 21:24:27 +0300

    Created game start screen

---
+++ src/js/game-start.js
@@ -0,0 +1,43 @@
+class GameStartScreen extends PIXI.Container {
+    #app;
+    #stage;
+    #view;
+
+    constructor({ app }) {
+        super();
+        this.#app = app;
+        this.#stage = app.stage;
+        this.#view = app.view;
+
+        const canvasWidth = this.#view.width;
+        const canvasHeight = this.#view.height;
+
+        this.position.set(0, 0);
+        this.width = canvasWidth;
+        this.height = canvasHeight;
+        this.visible = false;
+
+        const text = new PIXI.Text("CLICK   TO   START");
+        text.anchor.set(.5, 0);
+        text.position.set(canvasWidth / 2, canvasHeight * .3);
+        text.style = new PIXI.TextStyle({
+            fontFamily: "Arcade Classic",
+            fontSize: 35,
+            fill: 0x0A0A5D,
+        });
+
+        this.addChild(text);
+    }
+
+    show() {
+        this.visible = true;
+        this.#stage.addChild(this);
+    }
+
+    hide() {
+        this.visible = false;
+        this.#stage.removeChild(this);
+    }
+}
+
+export default GameStartScreen;
