bfa312f1e511224d1997ccc6b822253febf52288
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2024-03-31 21:30:42 +0300

    Added init and destroy methods and cancelation logic of the spawning interval when applicaiton is paused (tab out of focus).

--- src/js/spawner.js
+++ src/js/spawner.js
@@ -1,19 +1,42 @@
 class Spawner {
+    #startFn;
+    #timer;
 
-    constructor({ factoryFn, maxSpawnCount = 30 }) {
-        const spawnInterval = 1000;
+    constructor({ app, factoryFn, maxSpawnCount = 30 }) {
+        const interval = 1000;
         this.maxSpawnCount = maxSpawnCount;
         this.factoryFn = factoryFn;
         this.spawns = [];
-        setInterval(() => {
-            this.spawn();
-        }, spawnInterval);
+        this.#startFn = () => {
+            this.#timer = window.setInterval(() => {
+                if (app.paused) {
+                    window.clearInterval(this.#timer);
+                    return;
+                }
+                
+                this.spawn();
+            }, interval);
+        };
+    }
+
+    init() {
+        this.#startFn?.();
+    }
+
+    destroy() {
+        if (this.#timer) {
+            window.clearInterval(this.#timer);
+        }
+
+        this.spawns.forEach(spawn => spawn.destroy?.());
+        this.spawns.length = 0;
     }
 
     spawn() {
         if (this.spawns.length >= this.maxSpawnCount) return;
 
         let s = this.factoryFn();
+        s.init();
         this.spawns.push(s);
     }
 }
