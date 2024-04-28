b55c9a896953a5696101c1ad6cc798e5619f58f0
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2024-04-28 04:10:51 +0300

    Added loader and changed app options to resize to the parent wrapper element; Save max score to localStorage.

--- main.js
+++ main.js
@@ -5,16 +5,23 @@ import Player from "./src/js/player.js";
 import Zombie from "./src/js/zombie.js";
 import Spawner from "./src/js/spawner.js";
 
-const canvasSize = 512;
-const canvas = document.getElementById("app");
+const wrapper = document.querySelector('.wrapper');
+const canvas = wrapper.querySelector('canvas');
 
 const ticker = PIXI.Ticker.shared;
 const app = new PIXI.Application({
     view: canvas,
-    width: canvasSize,
-    height: canvasSize,
-    backgroundColor: 0x5c812f,
+    backgroundColor: 0x000000,
+    antialias: true, 
+    transparent: false, 
+    resolution: 1,
+    resizeTo: wrapper
 });
+
+app.loader
+    .add('PressStart2P', './src/assets/fonts/PressStart2P/PressStart2P_Regular.ttf')
+    .load(onLoadCompleted);
+
 const stage = app.stage;
 const gameOverScreen = new GameOverScreen({ app });
 const gameStartScreen = new GameStartScreen({ app });
@@ -26,31 +33,33 @@ let started = false;
 let score = 0;
 let maxScore = 0;
 
-gameStartScreen.show();
-ticker.autoStart = false;
-ticker.add(onUpdate);
-
-window.addEventListener('focus', function () {
-    if (started) {
-        // console.log('Game resumed');
-        app.paused = false;
-        zSpawner.init();
-        ticker.start();
-    }
+function onLoadCompleted() {
+    gameStartScreen.show();
+    ticker.autoStart = false;
+    ticker.add(onUpdate);
     
-});
-window.addEventListener('blur', function () {
-    if (started) {
-        // console.log('Game puased');
-        app.paused = true;
-        ticker.stop();
-    }
-});
+    window.addEventListener('focus', function () {
+        if (started) {
+            app.paused = false;
+            zSpawner.init();
+            ticker.start();
+        }       
+    });
 
-document.addEventListener("click", start, { once: true });
+    window.addEventListener('blur', function () {
+        if (started) {
+            app.paused = true;
+            ticker.stop();
+        }
+    });
+    
+    document.addEventListener("click", start, { once: true });
+}
 
 function start() {
     score = 0;
+    let maxScoreFromCache = window.localStorage.getItem('highScore');
+    maxScore = (maxScoreFromCache && parseInt(maxScoreFromCache)) || 0;
     started = true;
 
     if (gameStartScreen.visible) {
@@ -71,6 +80,7 @@ function stop() {
     started = false;
     if (score > maxScore) {
         maxScore = score;
+        window.localStorage.setItem('highScore', maxScore);
     }
     // console.log('You score: ', score);
     // console.log('Max score: ', maxScore);
@@ -80,7 +90,10 @@ function stop() {
     zSpawner.destroy();
     player.destroy();
     gameOverScreen.show({ score, maxScore });
-    document.addEventListener("click", start, { once: true });
+    let timeoutId = window.setTimeout(function () {
+        window.clearInterval(timeoutId);
+        document.addEventListener("click", start, { once: true });
+    }, 1500);
 }
 
 function onUpdate(dt) {
@@ -92,6 +105,7 @@ function onUpdate(dt) {
     }
 
     player.update(dt);
+    zSpawner.spawns = zSpawner.spawns.filter(zombie => zombie.visible);
     zSpawner.spawns.forEach(zombie => zombie.update(dt));
     if (bulletHitTest({ bullets: player.shooter.bullets, zombies: zSpawner.spawns })) {
         score += 1;
@@ -108,10 +122,11 @@ function bulletHitTest({ bullets, zombies }) {
             const hasHit = distance < bullet.halfWidth + zombie.halfWidth;
 
             if (hasHit) {
+                bullet.visible = false;
                 bullet.destroy();
+
+                zombie.visible = false;
                 zombie.kill();
-                bullets.splice(i, 1);
-                zombies.splice(j, 1);
             }
 
             return hasHit;
